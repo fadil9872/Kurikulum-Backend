d8251ef8e1ebdd5e47600fc006d88ae334ca2828
Ahmat Arif Ilhaka <ahmadarifilahaka77@gmail.com>
2020-09-09 09:19:08 +0700

    add Topik-3

--- Sprint-1/Topik-2/Tugas-2.2/Latihan-2.2.3.php
+++ Sprint-1/Topik-2/Tugas-2.2/Latihan-2.2.3.php
@@ -1,15 +1,24 @@
 <?php
-    $celsius = 34;
+    echo "Inputkan Suhu Celsius yang ingin dikonversi : ";
+    $celsius = trim(fgets(STDIN));
 
-    $kelvin = $celsius+273;
-    $fahrenheit = ((9/5)*$celsius)+32;
-    $reaumur = (4/5)*$celsius;
+    echo "1. Kelvin \n2. Fahrenhiet \n3.Reaumur \n";
+    echo "Pilih konversi : ";
+    $pilih = trim(fgets(STDIN));
+    switch($pilih){
+        case 1 : 
+            $hasil = $celsius+273;
+            echo "Konver Celsius = ".$celsius." ke Kelvin = ".$hasil."\n"; 
+            break;
+        case 2 : 
+            $hasil = ((9/5)*$celsius)+32; 
+            echo "Konver Celsius = ".$celsius." ke Fahrenhiet = ".$hasil."\n";
+            break;
+        case 3 : 
+            $hasil = (4/5)*$celsius;
+            echo "Konver Celsius = ".$celsius." ke Reaumur = ".$hasil."\n"; 
+            break;
+    }
 
-    echo "Suhu Celsius : ".$celsius;
-    echo "<br>";
-    echo "Konversi ke Kelvin : ".$kelvin;
-    echo "<br>";
-    echo "Konversi ke Fahrenheit : ".$fahrenheit;
-    echo "<br>";
-    echo "Konversi ke Reaumur : ".$reaumur;
+    
 ?>
---
+++ Sprint-1/Topik-3/Latihan-3.1/Latihan-3.1.2.php
@@ -0,0 +1,10 @@
+<?php
+    $n = 9;
+    for ($i=1; $i <$n ; $i++) { 
+        if($i%2==0){
+            continue;
+        }
+        echo $i." ";
+    }
+    echo "\n";
+?>
