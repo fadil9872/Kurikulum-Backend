9ad44f8a8fed1a230b78dce0e305154941a56562
Ahmat Arif Ilhaka <ahmadarifilahaka77@gmail.com>
2020-09-10 10:46:49 +0700

    update latihan-4.1.2.php

--- Sprint-1/Topik-4/latihan-4.1.2.php
+++ Sprint-1/Topik-4/latihan-4.1.2.php
@@ -1,14 +1,30 @@
 <?php
-    // $bil = [5,9,6,7,9,8,10,7,8];
-    echo "Ingin input berapa angka ? ";
-    $input = trim(fgets(STDIN));
-    $bil = array();
-    for($i=0;$i<$input; $i++){
-        echo "Nilai ke-".$i.": "; 
-        array_push($bil,trim(fgets(STDIN)));
+    $bil = [5,9,6,7,9,8,10,7,8];
+    // echo "Ingin input berapa angka ? ";
+    // $input = trim(fgets(STDIN));
+    // $bil = array();
+    // for($i=0;$i<$input; $i++){
+    //     echo "Nilai ke-".$i.": "; 
+    //     array_push($bil,trim(fgets(STDIN)));
+    // }
+    arsort($bil);
+    // print_r($bil);
+    $sort = array();
+    $i=0;
+    foreach($bil as $k => $v){
+        $sort[$i]=$v;
+        $i++;
     }
-    $max = max($bil);
-    $min = min($bil);
-
-    echo "Nilai Maksimal = ".$max.", Nilai Minimal = ".$min."\n";
+    print_r($sort);
+    echo "Nilai Max : ";
+    for($i=0;$i<3;$i++){
+        echo $sort[$i]." ";
+    }
+    echo "\n";
+    echo "Nilai Min : ";
+    for($i=count($sort)-1;$i>(count($sort)-4);$i--){
+        echo $sort[$i]." ";
+    }
+    echo "\n";
+    // echo "Nilai Maksimal = ".$max.", Nilai Minimal = ".$min."\n";
 ?>
