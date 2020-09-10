f4ec7698735fcaf0f29f0b5876866980bad4d0cf
Ahmat Arif Ilhaka <ahmadarifilahaka77@gmail.com>
2020-09-10 09:37:31 +0700

    upadte latihan-4

--- Sprint-1/Topik-4/latihan-4.1.1.php
+++ Sprint-1/Topik-4/latihan-4.1.1.php
@@ -1,12 +1,12 @@
 <?php
-    $bil = [5,9,6,7,9,8,10,7];
-    // echo "Ingin input berapa angka ? ";
-    // $input = trim(fgets(STDIN));
-    // $bil = array();
-    // for($i=0;$i<$input; $i++){
-    //     echo "Nilai ke-".$i.": "; 
-    //     array_push($bil,trim(fgets(STDIN)));
-    // }
+    // $bil = [5,9,6,7,9,8,10,7,8];
+    echo "Ingin input berapa angka ? ";
+    $input = trim(fgets(STDIN));
+    $bil = array();
+    for($i=0;$i<$input; $i++){
+        echo "Nilai ke-".$i.": "; 
+        array_push($bil,trim(fgets(STDIN)));
+    }
     echo "Niali ";
     foreach ($bil as $key => $value) {
         echo "{$value}";
--- Sprint-1/Topik-4/latihan-4.1.2.php
+++ Sprint-1/Topik-4/latihan-4.1.2.php
@@ -1,5 +1,12 @@
 <?php
-    $bil = [5,9,6,7,9,8,10,7,8];
+    // $bil = [5,9,6,7,9,8,10,7,8];
+    echo "Ingin input berapa angka ? ";
+    $input = trim(fgets(STDIN));
+    $bil = array();
+    for($i=0;$i<$input; $i++){
+        echo "Nilai ke-".$i.": "; 
+        array_push($bil,trim(fgets(STDIN)));
+    }
     $max = max($bil);
     $min = min($bil);
 
