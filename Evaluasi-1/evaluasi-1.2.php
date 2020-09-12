738f1f84d0994859d8369024161c75b116074443
Ahmat Arif Ilhaka <ahmadarifilahaka77@gmail.com>
2020-09-12 07:26:15 +0700

    update evaluasi-1.2

--- Evaluasi-1/evaluasi-1.2.php
+++ Evaluasi-1/evaluasi-1.2.php
@@ -1,20 +1,23 @@
 <?php
     $bil = 5956560159466056;
-    $arr_bil = explode('0',(string)$bil);
-    // print_r($arr_bil);
-    foreach($arr_bil as $key => $value){
-        $bil2[] = str_split($arr_bil[$key],1);
-      
-    }
-    foreach($bil2 as $key =>$value){
-        sort($bil2[$key]);
-    }
-    $hasil = "";
-    for($i=0;$i<count($bil2);$i++){
-        for($j=0;$j<count($bil2[$i]);$j++){
-            $hasil .= $bil2[$i][$j];
+    function selectionValue($bil){
+        $arr_bil = explode('0',(string)$bil);
+        // print_r($arr_bil);
+        foreach($arr_bil as $key => $value){
+            $bil2[] = str_split($arr_bil[$key],1);
+        
         }
+        foreach($bil2 as $key =>$value){
+            sort($bil2[$key]);
+        }
+        $hasil = "";
+        for($i=0;$i<count($bil2);$i++){
+            for($j=0;$j<count($bil2[$i]);$j++){
+                $hasil .= $bil2[$i][$j];
+            }
+        }
+        return $hasil;
     }
-    echo $hasil."\n";
+    echo selectionValue($bil)."\n";
 
 ?>
