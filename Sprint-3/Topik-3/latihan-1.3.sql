a6c9aeb507ccd7588e01b1a5f157afe64f6b0485
ilahaka77 <ahmadarifilahaka77@gmail.com>
2020-09-24 10:57:14 +0700

    add tugas-3

---
+++ Sprint-3/Topik-3/latihan-1.3.sql
@@ -0,0 +1,160 @@
+-- MySQL dump 10.13  Distrib 5.7.31, for Linux (x86_64)
+--
+-- Host: localhost    Database: sprint-3
+-- ------------------------------------------------------
+-- Server version	5.7.31-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `detail_transaksi`
+--
+
+DROP TABLE IF EXISTS `detail_transaksi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `detail_transaksi` (
+  `kd_transaksi` char(10) DEFAULT NULL,
+  `kd_barang` char(10) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  KEY `kd_transaksi` (`kd_transaksi`),
+  KEY `kd_barang` (`kd_barang`),
+  CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`kd_transaksi`) REFERENCES `tb_transaksi` (`kd_transaksi`),
+  CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`kd_barang`) REFERENCES `tb_barang` (`kd_barang`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detail_transaksi`
+--
+
+LOCK TABLES `detail_transaksi` WRITE;
+/*!40000 ALTER TABLE `detail_transaksi` DISABLE KEYS */;
+INSERT INTO `detail_transaksi` VALUES ('T001','B001',2),('T001','B002',2),('T001','B003',2),('T002','B001',3),('T002','B002',3),('T002','B003',3),('T003','B001',4),('T003','B002',4),('T003','B003',4);
+/*!40000 ALTER TABLE `detail_transaksi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_barang`
+--
+
+DROP TABLE IF EXISTS `tb_barang`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_barang` (
+  `kd_barang` char(10) NOT NULL,
+  `nama_barang` varchar(100) DEFAULT NULL,
+  `harga` double DEFAULT NULL,
+  `supplier` char(10) DEFAULT NULL,
+  PRIMARY KEY (`kd_barang`),
+  KEY `supplier` (`supplier`),
+  CONSTRAINT `tb_barang_ibfk_1` FOREIGN KEY (`supplier`) REFERENCES `tb_supplier` (`id_supplier`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+LOCK TABLES `tb_barang` WRITE;
+/*!40000 ALTER TABLE `tb_barang` DISABLE KEYS */;
+INSERT INTO `tb_barang` VALUES ('B001','Sabun Mandi',6000,'S001'),('B002','Pasta Gigi',4000,'S002'),('B003','Sampo',8000,'S003');
+/*!40000 ALTER TABLE `tb_barang` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_pegawai`
+--
+
+DROP TABLE IF EXISTS `tb_pegawai`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_pegawai` (
+  `id_pegawai` char(10) NOT NULL,
+  `nama_pegawai` varchar(100) DEFAULT NULL,
+  `posisi` varchar(30) DEFAULT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_pegawai`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_pegawai`
+--
+
+LOCK TABLES `tb_pegawai` WRITE;
+/*!40000 ALTER TABLE `tb_pegawai` DISABLE KEYS */;
+INSERT INTO `tb_pegawai` VALUES ('K001','Agung Sejagat','Kasir','Karanganyar'),('K002','Damar Jati','Kasir','Bandungan');
+/*!40000 ALTER TABLE `tb_pegawai` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_supplier`
+--
+
+DROP TABLE IF EXISTS `tb_supplier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_supplier` (
+  `id_supplier` char(10) NOT NULL,
+  `nama_suppler` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_supplier`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_supplier`
+--
+
+LOCK TABLES `tb_supplier` WRITE;
+/*!40000 ALTER TABLE `tb_supplier` DISABLE KEYS */;
+INSERT INTO `tb_supplier` VALUES ('S001','PT. Sabun Mandi'),('S002','PT. Pasta Gigi'),('S003','PT. Sampo');
+/*!40000 ALTER TABLE `tb_supplier` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_transaksi`
+--
+
+DROP TABLE IF EXISTS `tb_transaksi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_transaksi` (
+  `kd_transaksi` char(10) NOT NULL,
+  `tgl_transaksi` date DEFAULT NULL,
+  `id_pegawai` char(10) DEFAULT NULL,
+  PRIMARY KEY (`kd_transaksi`),
+  KEY `id_pegawai` (`id_pegawai`),
+  CONSTRAINT `tb_transaksi_ibfk_1` FOREIGN KEY (`id_pegawai`) REFERENCES `tb_pegawai` (`id_pegawai`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_transaksi`
+--
+
+LOCK TABLES `tb_transaksi` WRITE;
+/*!40000 ALTER TABLE `tb_transaksi` DISABLE KEYS */;
+INSERT INTO `tb_transaksi` VALUES ('T001','2020-09-19','K001'),('T002','2020-09-18','K001'),('T003','2020-09-18','K002');
+/*!40000 ALTER TABLE `tb_transaksi` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-09-24 10:50:05
